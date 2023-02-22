e67d58a226bd618fa23450fdab14cf1d73945746
Edwin L. Jaya <edwinkudo.07@gmail.com>
2023-02-22 19:41:48 +0700

    Nambah server flask, fix nama model

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl','rb'))
+
+@app.route('/')
+def home():
+    return "hello World"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    company = request.form.get('company')
+    cpu = request.form.get('cpu')
+    ram = request.form.get('ram')
+    gpu = request.form.get('gpu')
+    opsys = request.form.get('opsys')
+    weight = request.form.get('weight')
+    hdd = request.form.get('hdd')
+    ssd = request.form.get('ssd')
+
+    kumpulan_data_queri = numpy.array([[company,cpu,ram,gpu,opsys,weight,hdd,ssd]])
+    harga      = int(numpy.exp(model.predict(kumpulan_data_queri)[0]))
+    konversi_harga      = harga*16204
+    prediction = str(konversi_harga)
+    hasil = {
+        "company":company,
+        "cpu":cpu,
+        "ram":ram,
+        "gpu":gpu,
+        "Operating System":opsys,
+        "Weight":weight,
+        "HDD":hdd,
+        "SSD":ssd
+    }
+    return jsonify({'harga':prediction})
+
+if __name__ == "__main__":
+    app.run(debug=True)
