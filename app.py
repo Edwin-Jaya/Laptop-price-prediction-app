d7b8c3038c8a0c8bc33c0d8ba4d4c0a39e732514
Edwin L. Jaya <edwinkudo.07@gmail.com>
2023-02-23 20:41:42 +0700

    Benerin web

--- app.py
+++ app.py
@@ -36,5 +36,23 @@ def predict():
     }
     return jsonify({'harga':prediction})
 
+@app.route('/predictharga', methods=['POST'])
+def predictharga():
+    cmpn = str.capitalize((request.form.get('company')))
+    cpu = str(request.form.get('cpu'))
+    ram = int(request.form.get('ram'))
+    gpu = str(request.form.get('gpu'))
+    opsys = str(request.form.get('opsys'))
+    weight = float(request.form.get('weight'))
+    hdd = int(request.form.get('hdd'))
+    ssd = int(request.form.get('ssd'))
+
+    kumpulan_data_queri = numpy.array([[cmpn,cpu,ram,gpu,opsys,weight,hdd,ssd]])
+    harga      = int(numpy.exp(model.predict(kumpulan_data_queri)[0]))
+    konversi_harga      = harga*16204
+    prediction = int(konversi_harga)
+    return render_template('hasil.html', prediction_text='Rp{:,}'.format(prediction).replace(',','.'))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
