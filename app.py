ee648413b68a22dd0a34928659f380b45da1c79d
Edwin L. Jaya <edwinkudo.07@gmail.com>
2023-02-23 13:57:54 +0700

    benerin kode web

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 import numpy
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./templates/', static_folder='./templates/static/')
 model = pickle.load(open('./model/model.pkl','rb'))
 
 @app.route('/')
